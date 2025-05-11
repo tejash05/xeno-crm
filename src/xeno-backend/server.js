c781ad8addc6f1cf70f5906a4a4b202bcb3ce3d5
Tejash Tarun <tejashtarun@Tejashs-MacBook-Air-6.local>
2025-05-11 08:23:46 +0530

    Update server.js and other changes

--- src/xeno-backend/server.js
+++ src/xeno-backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // ✅ Proper CORS configuration
 app.use(cors({
-  origin: 'https://xeno-frontend-3qja.onrender.com/',
+  origin: 'https://xeno-frontend-3qja.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH' ,  'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
